147ce514eda9238a083b94e2ada893e356799a1d
Gabriel <103146230+JLFrozy@users.noreply.github.com>
2025-10-15 20:17:18 +0200

    add getWeatherByLocation Controller

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -19,3 +19,22 @@ const getCurrentWeather = async (req, res) => {
     }
 }
 
+const getWeatherByLocation = async (req, res) => {
+    try{
+        const { lat, lon} = req.query;
+        if (!lat || !lon) {
+            return res.status(400).json({ message: 'Veuillez spécifier des coordonnées' });
+        }
+        const weatherData = await weatherService.getWeatherByCoordinates(lat, lon);
+        res.status(200).json({
+            success: true,
+            data: weatherData
+        })
+    } catch (error){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
